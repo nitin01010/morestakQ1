dfe7c0a8b1e2bea7af9f549067e5b312a4941f21
nitin0202 <nitinsachdeva0202@gmail.com>
2024-06-18 12:56:44 +0530

    add fiex some bug

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes/routes"));
-app.use(require("./db/dbConnect"));
+require("./db/dbConnect");
 
 app.listen(Port, () => {
     console.log(`server is working ${Port}`);
